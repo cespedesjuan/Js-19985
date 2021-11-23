d483973f7ec44f7e32ced19c287316d0d785dfde
cespedesjuan <cespedesjuanpablo01@gmail.com>
2021-11-23 11:17:08 -0300

    ENTREGA NR 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-let nombreIngresado = prompt("Ingrese su nombre")
-//let apellidoIngresado = prompt("Ingrese su apellido") 
-//let edadIngresada = prompt("Ingrese su edad") 
-
-
-console.log("Tu nombre es" + " " + nombreIngresado + " " + apellidoIngresado + " y tenes " + edadIngresada + " años")
+let username = prompt("Ingrese su username (MÍNIMO 8 CARACTERES)");
+if (username.length < 8) {
+    alert("Tu usuario debe contener mas de 8 caracteres, vuelva a intentar")
+    alert("Login fallido, vuelva a intentar")
+}
+else if (username.length > 7) {
+    let pin = prompt("Ingrese su PIN (6 O MÁS NUMEROS UNICAMENTE)");
+    if (pin.length < 6) {
+        alert("Tu PIN debe contener más de 6 números")
+        alert("Login fallido, vuelva a intentar")
+    }
+    else if (pin.length > 5) {
+        alert("Login exitoso")
+    }
+}
