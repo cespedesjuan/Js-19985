d3ee0fee836a801339199270283d50c4dd303396
cespedesjuan <cespedesjuanpablo01@gmail.com>
2021-11-28 15:06:39 -0300

    4ta entrega

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+//* ENTREGABLE NR 3
+function ejercicioFor() {
+    let numero = prompt ("Ingrese un numero del 1 al 10");
+    parseInt(numero, 10)
+    for (numero; numero > 0; numero--)
+    alert("Clickeame " + numero + " veces más" )
+}
+
+
 function Login() {
   let username = prompt("Ingrese su nombre de usuario (MÍNIMO 8 CARACTERES)");
   if (username.length < 8) {
@@ -15,11 +24,32 @@ function Login() {
     }
 }
 
-function ejercicioFor() {
-    let numero = prompt ("Ingrese un numero del 1 al 10");
-    parseInt(numero, 10)
-    for (numero; numero > 0; numero--)
-    alert("Clickeame " + numero + " veces más" )
+//* Desafío complementario  con 3 funciones adjuntas
+
+function esPar (num) {
+    num = prompt("Ingresa un numero")
+    parseInt(num, 10)
+    if (num % 2 == 0){
+    alert("Es par")
+    }
+    else {
+        alert("Es impar")
+    }
 }
 
-ejercicioFor()
+function impuestoGamer (num) {
+    num = prompt("Ingresa el precio del juego que querés comprar")
+    resultado = parseInt(num,10) + (parseInt(num,10) * 0.65)
+    alert(resultado)
+}
+
+function promedio (a,b,c) {
+    a= prompt("Ingresa primera nota")
+    b= prompt("Ingresa segunda nota")
+    c= prompt("Ingresa tercera nota")
+    resultado = (parseInt(a,10) + parseInt(b,10) + parseInt(c,10)) / 3
+    alert("Tu promedio es de " + resultado)
+}
+esPar()
+impuestoGamer()
+promedio()
