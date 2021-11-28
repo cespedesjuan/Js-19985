5773c40fa2737acb715d1d00dd0fb6d28309c551
cespedesjuan <cespedesjuanpablo01@gmail.com>
2021-11-28 14:01:09 -0300

    ENTREGA NR 3

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
-let username = prompt("Ingrese su username (MÍNIMO 8 CARACTERES)");
-if (username.length < 8) {
-    alert("Tu usuario debe contener mas de 8 caracteres, vuelva a intentar")
-    alert("Login fallido, vuelva a intentar")
-}
-else if (username.length > 7) {
-    let pin = prompt("Ingrese su PIN (6 O MÁS NUMEROS UNICAMENTE)");
-    if (pin.length < 6) {
-        alert("Tu PIN debe contener más de 6 números")
-        alert("Login fallido, vuelva a intentar")
+function Login() {
+  let username = prompt("Ingrese su nombre de usuario (MÍNIMO 8 CARACTERES)");
+  if (username.length < 8) {
+    return alert(
+      "Tu usuario tiene que tener 8 carácteres mínimo, volvé a intentarlo."
+    );
+  } else if (username.length > 7) {
+        let pin = prompt("Ingrese su PIN (MÍNIMO 6 NÚMEROS)");
+        if (pin.length < 6) {
+            return alert("Tu PIN tiene estar formado por 6 números");
+        }
+        else if (pin.length >= 6); {
+            alert("Login exitoso")
+        }
     }
-    else if (pin.length > 5) {
-        alert("Login exitoso")
-    }
 }
+
+function ejercicioFor() {
+    let numero = prompt ("Ingrese un numero del 1 al 10");
+    parseInt(numero, 10)
+    for (numero; numero > 0; numero--)
+    alert("Clickeame " + numero + " veces más" )
+}
+
+ejercicioFor()
